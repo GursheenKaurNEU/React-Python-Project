030a0de8924b433d2cc3f3210a3ecfd63cbefde1
Gursheen Kaur <kaur.gurs@northeastern.edu>
2023-05-10 17:00:08 -0400

    Adding Image card and delete image functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,39 +1,58 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header";
 import Search from "./components/Search";
+import ImageCard from "./components/ImageCard";
 import { useState } from "react";
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
 
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 const App = () => {
   const [word, setWord] = useState("");
-  const [images,setImages] = useState([])
-  console.log(images)
-  const handleSearchSubmit = async(e) => { 
+  const [images, setImages] = useState([]);
+  console.log(images);
+  const handleSearchSubmit = async (e) => {
     e.preventDefault();
     console.log(word);
 
-    const response =  await fetch( //fetch returns a promise
+    const response = await fetch(
+      //fetch returns a promise
       `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`
-    )
-    const data = await response.json()
-    setImages([data,...images])
-    setWord('')
+    );
+    const data = await response.json();
+    setImages([{ ...data, title: word }, ...images]);
+    setWord("");
 
-      // .then((res) => res.json())
-      // .then((data) => {
-      //   console.log(data);
-      // })
-      // .catch((err) => {
-      //   console.log(err);
-      // });
+    // .then((res) => res.json())
+    // .then((data) => {
+    //   console.log(data);
+    // })
+    // .catch((err) => {
+    //   console.log(err);
+    // });
   };
-  
+
+  const handleDelete = (id) => {
+    setImages(images.filter((image) => image.id != id))
+  }
+
   //console.log(word); //anytime when the state of the component is change, the component is rerendered
   return (
     <div className="App">
       <Header title="Images Gallery" />
       <Search setWord={setWord} word={word} handleSubmit={handleSearchSubmit} />
+      {/* { !!images.length && <ImageCard images={
+        images[0]
+      } />} */}
+      <Container className="mt-4">
+        <Row xs={1} lg={3} md={2}>
+          {images.map((image, i) => (
+            <Col className="pb-3" key={i}>
+              <ImageCard images={image}  deleteImage ={handleDelete}/>
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </div>
   );
 };
---
+++ frontend/src/components/ImageCard.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Card,Button } from "react-bootstrap";
+
+const ImageCard = ({images,deleteImage}) => {
+    console.log(images)
+  return (
+    <Card style={{ width: "18rem" }}>
+      <Card.Img variant="top" src={images.urls.small} />
+      <Card.Body>
+        <Card.Title>{images.title.toUpperCase()}</Card.Title>
+        <Card.Text>
+        {images.description ? images.description : images.alt_description }
+        </Card.Text>
+        <Button variant="primary" onClick = { () => deleteImage(images.id)} >Delete</Button>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default ImageCard;
