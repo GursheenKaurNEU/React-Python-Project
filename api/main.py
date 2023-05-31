d6f8ef04f93f9dde9f8ddb98619e5208fa8fad27
Gursheen Kaur <kaur.gurs@northeastern.edu>
2023-05-31 00:17:48 -0400

    Adding api endpoints for getting and saving images

--- api/main.py
+++ api/main.py
@@ -1,11 +1,15 @@
-from flask import Flask,request;
+from flask import Flask,request,jsonify;
 from flask_cors import CORS;
 import requests
 from dotenv import load_dotenv
 import os
-from mongo_client import insert_test_document
+# from mongo_client import insert_test_document
+from mongo_client  import mongoClient
 
 
+
+gallery = mongoClient.gallery
+images_collection = gallery.images
 
 load_dotenv(dotenv_path="./.env.local")
 # print(os.environ.get('REACT_APP_UNSPLASH_KEY')) 
@@ -20,7 +24,7 @@ if not REACT_APP_UNSPLASH_KEY:
 
 app= Flask(__name__)
 app.config['DEBUG'] = DEBUG
-insert_test_document()
+
 CORS(app)
 # print(__name__)
 
@@ -41,6 +45,23 @@ def new_image():
     }
     response = requests.get(url=UNSPLASH_API_URL, headers=headers,params=params)
     return response.json()
+
+
+
+@app.route('/images',methods=["GET","POST"])
+def images():
+    if request.method == 'GET':
+        # read images from the db
+        images =images_collection.find({})
+        return jsonify([img for img in images])
+    if request.method == 'POST':
+        # save image in the db 
+        image = request.get_json();
+        image["_id"] = image.get("id")
+        result = images_collection.insert_one(image)
+        insertedId= result.inserted_id
+        return {"inserted_id":insertedId}
+        
 
 if __name__ == "__main__" :
     app.run(host="0.0.0.0", port=5050)
